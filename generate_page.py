64ee02bac5d94231721a20446e4796638e8b6a78
Robert Kirk <robert.kirk.3.14@gmail.com>
2022-06-06 17:46:02 +0100

    Update arxiv api call, and add semantic scholar functionality

--- generate_page.py
+++ generate_page.py
@@ -4,6 +4,7 @@ import openreview
 from time import sleep
 
 import arxiv
+from semanticscholar import SemanticScholar
 import click
 import pyperclip
 from jinja2 import Template
@@ -12,6 +13,8 @@ from jinja2 import Template
 ARXIV_ID_REGEX = re.compile(r"https:\/\/(ar5iv\.labs\.)?arxiv.org\/((abs)|(pdf)|(html))\/(\d{3,5}\.\d{3,5})(\.pdf)?")
 
 OPENREVIEW_ID_REGEX = re.compile(r"https:\/\/openreview.net/((forum)|(pdf))\?id\=(\w*)")
+
+SEMANTICSCHOLAR_ID_REGEX = re.compile(r"https:\/\/www.semanticscholar.org\/paper\/.*\/(\w*)")
 
 
 def get_template():
@@ -23,16 +26,16 @@ def get_template():
 def arxiv_create_page_dict(arxiv_id, arxiv_url):
     if arxiv_id is None and arxiv_url is not None:
         arxiv_id = ARXIV_ID_REGEX.match(arxiv_url).groups()[5]
-    response = arxiv.query(id_list=[str(arxiv_id)])
-    arxiv_obj = response[0]
+    response = arxiv.Search(id_list=[str(arxiv_id)]).results()
+    arxiv_obj = next(response)
     page_dict = dict(
         url=f"https://arxiv.org/abs/{arxiv_id}",
         pdf_url=f"https://arxiv.org/pdf/{arxiv_id}.pdf",
-        authors=arxiv_obj["authors"],
-        published_year=arxiv_obj["published"][:4],
-        published_month=arxiv_obj["published"][5:7],
-        abs=arxiv_obj["summary_detail"]["value"].replace("\n", " "),
-        title=arxiv_obj["title"].replace("\n", " ").replace("  ", " ").replace("  ", " "),
+        authors=arxiv_obj.authors,
+        published_year=arxiv_obj.published.year,
+        published_month=arxiv_obj.published.strftime('%m'),
+        abs=arxiv_obj.summary.replace("\n", " "),
+        title=arxiv_obj.title.replace("\n", " ").replace("  ", " ").replace("  ", " "),
     )
     return page_dict
 
@@ -51,6 +54,23 @@ def openreview_create_page_dict(openreview_id, openreview_url):
         published_month=created.strftime("%m"),
         abs=openreview_obj.content["abstract"],
         title=openreview_obj.content["title"].replace("\n", " ").replace("  ", " ").replace("  ", " "),
+    )
+    return page_dict
+
+
+def semanticscholar_create_page_dict(semanticscholar_id, semanticscholar_url):
+    if semanticscholar_id is None and semanticscholar_url is not None:
+        semanticscholar_id = SEMANTICSCHOLAR_ID_REGEX.match(semanticscholar_url).groups()[0]
+    sch = SemanticScholar(timeout=10)
+    response = sch.paper(semanticscholar_id)
+    page_dict = dict(
+        url=response["url"],
+        pdf_url=f"https://www.semanticscholar.org/reader/{response['paperId']}",
+        authors=[a["name"] for a in response["authors"]],
+        published_year=response["year"],
+        published_month=response.get("month", "??"),
+        abs=response["abstract"],
+        title=response["title"],
     )
     return page_dict
 
@@ -75,6 +95,9 @@ def main(paper_id, paper_url):
         elif OPENREVIEW_ID_REGEX.match(clipboard_contents):
             paper_url = clipboard_contents
             page_dict = openreview_create_page_dict(paper_id, paper_url)
+        elif SEMANTICSCHOLAR_ID_REGEX.match(clipboard_contents):
+            paper_url = clipboard_contents
+            page_dict = semanticscholar_create_page_dict(paper_id, paper_url)
     template = get_template()
     page_output = write_page(page_dict, template)
     pyperclip.copy(page_output)
